ca4f52aae8a949bddb526beab4a339207a4b3cb1
sararuiza <scamila.ruiz@udea.edu.co>
2024-02-01 13:01:35 -0500

    pruebas completadas del loggin funcionalidad 100%

--- Modules/Olvidocontra/appOC.js
+++ Modules/Olvidocontra/appOC.js
@@ -1,3 +1,6 @@
+import { patchUser, getUsersbyEmail } from "../CRUD_USUARIOS/users.js";
+import { avisos, clearDiv } from "../Alertas/alertas.js";
+
 //SELECTORES
 
 const formulario = document.querySelector(".form");
@@ -7,10 +10,16 @@ const camposForm = document.querySelectorAll(".form-control");
 const emailInput = document.querySelector("#exampleInputEmail1");
 const paswordInput = document.querySelector("#exampleInputPassword1");
 const paswordInput2 = document.querySelector("#exampleInputPassword2");
-const alerta = document.querySelector(".alerta");
+
+const regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const regexPassword =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&.]|[^ ]){8,15}$/;
+
+console.log(regexPassword);
+let alertContraExist = false;
 
 //EVENTOS
-btnEnviar.addEventListener("click", (event) => {
+btnEnviar.addEventListener("click", async (event) => {
   event.preventDefault();
   let registroUsuario = {
     email: "",
@@ -27,65 +36,108 @@ btnEnviar.addEventListener("click", (event) => {
   );
 
   if (!someIsInvalid) {
-    const regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     const validacionCorreo = regexCorreo.test(registroUsuario.email);
     if (!validacionCorreo) {
       avisos("Correo no valido", false);
     } else if (registroUsuario.contrasena !== registroUsuario.contrasena2) {
       avisos("Contraseñas no coinciden", false);
     } else {
-      const registroUsuarioString = JSON.stringify(registroUsuario);
-      console.log(registroUsuarioString);
+      debugger;
+      regexPassword.test(registroUsuario.contrasena2);
+      if (regexPassword.test(registroUsuario.contrasena)) {
+        const data = await getUsersbyEmail(registroUsuario.email);
+        const idUser = data[0].id;
+        if (data.length > 0) {
+          avisos("El correo ya esta registrado", true);
+          emailInput.classList.add("is-invalid");
+          emailInput.classList.remove("is-valid");
 
-      localStorage.setItem("usuario", registroUsuarioString);
-
-      avisos("Contraseña actualizada correctamente", true);
+          const registroUsuarioString = JSON.stringify(registroUsuario);
+          patchUser(idUser, registroUsuarioString);
+          setTimeout(() => {
+            window.location.href = "http://127.0.0.1:5500/";
+          }, 2000);
+          avisos("Usuario creado exitosamente", true);
+        } else {
+          avisos("El correo no esta registrado", false);
+          emailInput.classList.add("is-invalid");
+          emailInput.classList.remove("is-valid");
+        }
+      } else {
+        avisos(`La contrsaeña no tiene el formato correcto`, false);
+        if (!alertContraExist) {
+          const pieform = document.querySelector(
+            "body > main > section.login > div > form > div.pieForm"
+          );
+          const prueba = document.createElement("div");
+          prueba.classList.add("ocasionalAlert");
+          prueba.innerHTML = `
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">La contraseña debe tener:</p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">1  Mayuscula </p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">1  Caracter especial </p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">8  Caracteres Mínimo </p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">15 Caracteres Máximo </p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-3"> Las contraseñas deben coincidir</p>
+          `;
+          formulario.insertBefore(prueba, pieform);
+          alertContraExist = true;
+          debugger;
+        }
+      }
     }
   } else {
     avisos("Todos los campos son obligatorios", false);
   }
-  formulario.reset();
 });
 
-
-
-function avisos(texto, condicion) {
-  const alert = document.createElement(`div`);
-  const icono = document.createElement("i");
-  const alertTitulo = document.createElement("p");
-  const hr = document.createElement("hr");
-  alertTitulo.textContent = texto;
-  alert.classList.add("alert");
-  icono.classList.add("bx");
-  alert.setAttribute("role", "alert");
-  alertTitulo.classList.add("alert-heading");
-
-  if (condicion == true) {
-    alert.classList.add("alert-success");
-    icono.classList.add("bx-check-circle");
-    icono.classList.add("check");
-  } else {
-    alert.classList.add("alert-danger");
-    icono.classList.add("bx");
-    icono.classList.add("bx-x-circle");
-    icono.classList.add("check");
+paswordInput2.addEventListener("blur", () => {
+  if (alertContraExist) {
+    const passAreTheSame = paswordInput2.value === paswordInput.value;
+    const pass2Isvalid = regexPassword.test(paswordInput2.value);
+    if (passAreTheSame && pass2Isvalid) {
+      formulario.removeChild(document.querySelector(".ocasionalAlert"));
+      paswordInput.classList.add("isvalid");
+      paswordInput2.classList.add("isvalid");
+      alertContraExist = false;
+    }
   }
+});
 
-  clearDiv();
-  alert.appendChild(icono);
-  alert.appendChild(alertTitulo);
-  alert.appendChild(hr);
-  alerta.appendChild(alert);
+// function avisos(texto, condicion) {
+//   const alert = document.createElement(`div`);
+//   const icono = document.createElement("i");
+//   const alertTitulo = document.createElement("p");
+//   const hr = document.createElement("hr");
+//   alertTitulo.textContent = texto;
+//   alert.classList.add("alert");
+//   icono.classList.add("bx");
+//   alert.setAttribute("role", "alert");
+//   alertTitulo.classList.add("alert-heading");
 
-  setTimeout(() => {
-    alert.style.display = "none";
-  }, 2000);
-}
+//   if (condicion == true) {
+//     alert.classList.add("alert-success");
+//     icono.classList.add("bx-check-circle");
+//     icono.classList.add("check");
+//   } else {
+//     alert.classList.add("alert-danger");
+//     icono.classList.add("bx");
+//     icono.classList.add("bx-x-circle");
+//     icono.classList.add("check");
+//   }
 
-function clearDiv() {
-  while (alerta.firstChild) {
-    alerta.removeChild(alerta.firstChild);
-  }
-}
+//   clearDiv();
+//   alert.appendChild(icono);
+//   alert.appendChild(alertTitulo);
+//   alert.appendChild(hr);
+//   alerta.appendChild(alert);
 
+//   setTimeout(() => {
+//     alert.style.display = "none";
+//   }, 2000);
+// }
 
+// function clearDiv() {
+//   while (alerta.firstChild) {
+//     alerta.removeChild(alerta.firstChild);
+//   }
+// }
--- Modules/Registro/appR.js
+++ Modules/Registro/appR.js
@@ -1,7 +1,7 @@
 //SELECTORES
 
-import { postUser, getMultipleUsers } from "../CRUD_USUARIOS/users.js";
-import { avisos, clearDiv} from "../Alertas/alertas.js"
+import { postUser, getUsersbyEmail } from "../CRUD_USUARIOS/users.js";
+import { avisos, clearDiv } from "../Alertas/alertas.js";
 
 const formulario = document.querySelector(".form");
 const btnEnviar = document.querySelector(".btn-btn-primary");
@@ -13,9 +13,58 @@ const emailInput = document.querySelector("#exampleInputEmail1");
 const paswordInput = document.querySelector("#exampleInputPassword1");
 const paswordInput2 = document.querySelector("#exampleInputPassword2");
 
+const regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const regexPassword =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&.]|[^ ]){8,15}$/;
+
+let alertContraExist = false;
+
+emailInput.addEventListener("blur", () => {
+  const isValidCorreo = regexCorreo.test(emailInput.value);
+  if (isValidCorreo) {
+    emailInput.classList.remove("is-invalid");
+    emailInput.classList.add("is-valid");
+  } else {
+    emailInput.classList.add("is-invalid");
+    emailInput.classList.remove("is-valid");
+  }
+});
+
+paswordInput2.addEventListener("blur", () => {
+  debugger;
+  if (alertContraExist) {
+    const passAreTheSame = paswordInput2.value === paswordInput.value;
+    const pass2Isvalid = regexPassword.test(paswordInput2.value);
+    if (passAreTheSame && pass2Isvalid) {
+      formulario.removeChild(document.querySelector(".ocasionalAlert"));
+      paswordInput.classList.remove("is-invalid");
+      paswordInput.classList.add("is-valid");
+      paswordInput2.classList.remove("is-invalid");
+      paswordInput2.classList.add("is-valid");
+      alertContraExist = false;
+    }
+  } else {
+    const isValidpass2 = regexPassword.test(paswordInput2);
+    const pass1pass2Same = paswordInput2.value === paswordInput.value;
+
+    console.log(pass1pass2Same);
+
+    if (isValidpass2 && pass1pass2Same) {
+      paswordInput.classList.remove("is-invalid");
+      paswordInput.classList.add("is-valid");
+      paswordInput2.classList.remove("is-invalid");
+      paswordInput2.classList.add("is-valid");
+    } else {
+      paswordInput.classList.add("is-invalid");
+      paswordInput.classList.remove("is-valid");
+      paswordInput2.classList.add("is-invalid");
+      paswordInput2.classList.remove("is-valid");
+    }
+  }
+});
 
 //EVENTOS
-btnEnviar.addEventListener("click", (event) => {
+btnEnviar.addEventListener("click", async (event) => {
   event.preventDefault();
   let registroUsuario = {
     nombre: "",
@@ -25,9 +74,9 @@ btnEnviar.addEventListener("click", (event) => {
     contrasena2: "",
   };
 
-  registroUsuario.nombre = nombreInput.value;
-  registroUsuario.apellido = apellidosInput.value;
-  registroUsuario.email = emailInput.value;
+  registroUsuario.nombre = String(nombreInput.value).trim();
+  registroUsuario.apellido = String(apellidosInput.value).trim();
+  registroUsuario.email = String(emailInput.value).trim().toLocaleLowerCase();
   registroUsuario.contrasena = paswordInput.value;
   registroUsuario.contrasena2 = paswordInput2.value;
 
@@ -36,32 +85,51 @@ btnEnviar.addEventListener("click", (event) => {
   );
 
   if (!someIsInvalid) {
-    const regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     const validacionCorreo = regexCorreo.test(registroUsuario.email);
     if (!validacionCorreo) {
-      emailInput.classList.add('is-invalid')
+      emailInput.classList.add("is-invalid");
       avisos("Correo no valido", false);
     } else if (registroUsuario.contrasena !== registroUsuario.contrasena2) {
       avisos("Contraseñas no coinciden", false);
     } else {
-      const registroUsuarioString = JSON.stringify(registroUsuario);
-      postUser(registroUsuarioString);
-      console.log(registroUsuarioString);
+      if (regexPassword.test(registroUsuario.contrasena)) {
+        const data = await getUsersbyEmail(registroUsuario.email);
+        if (data.length > 0) {
+          avisos("El correo ya esta registrado", false);
+          emailInput.classList.add("is-invalid");
+          emailInput.classList.remove("is-valid");
+        } else {
+          const registroUsuarioString = JSON.stringify(registroUsuario);
+          postUser(registroUsuarioString);
 
-      // localStorage.setItem("usuario", registroUsuarioString);
+          avisos("Usuario creado exitosamente", true);
 
-      avisos("Usuario creado exitosamente", true);
+          setTimeout(() => {
+            window.location.href = "http://127.0.0.1:5500/";
+          }, 2000);
+        }
+      } else {
+        avisos(`La contrsaeña no tiene el formato correcto`, false);
+        if (!alertContraExist) {
+          const pieform = document.querySelector(
+            "body > main > section.login > div > form > div.pieForm"
+          );
+          const prueba = document.createElement("div");
+          prueba.classList.add("ocasionalAlert");
+          prueba.innerHTML = `
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">La contraseña debe tener:</p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">1  Mayuscula </p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">1  Caracter especial </p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">8  Caracteres Mínimo </p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-0">15 Caracteres Máximo </p>
+          <p class="link-danger link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mb-3"> Las contraseñas deben coincidir</p>
+          `;
+          formulario.insertBefore(prueba, pieform);
+          alertContraExist = true;
+        }
+      }
     }
   } else {
     avisos("Todos los campos son obligatorios", false);
   }
-  formulario.reset();
-});
-
-
-
-let data2 = getMultipleUsers();
-
-data2.then((res) => {
-  console.log(res);
 });
